139577b655cfd455dde8967fa7e02ca2dc61e4eb
Aleksa Arsic <aleksa.arsic.ftn@gmail.com>
2019-09-26 17:15:50 +0200

    ValueError
    Catch ValueError exceptions.

--- levelGenerator.py
+++ levelGenerator.py
@@ -6,13 +6,22 @@ if __name__ == "__main__":
 
     level = [[0 for _ in range(w)] for _ in range(h)]
 
-    n = int(input("Enter number of platforms: "))
+    try:
+        n = int(input("Enter number of platforms: "))
+    except ValueError:
+        print("*** Error! Enter integer values only!")
+        sys.exit()
 
     platforms = [0 for _ in range(n * 3)]
 
     for i in range(0, len(platforms), 3):
-        platforms[i], platforms[i + 1], platforms[i + 2] = map(int, input("Enter (x, y, len) for platform number {} : ".format(i // 3)).split())
-    
+        try:
+            platforms[i], platforms[i + 1], platforms[i + 2] = map(int, input("Enter (x, y, len) for platform number {} : ".format(i // 3)).split())
+        except ValueError:
+            print("*** Error! Enter integer values only!")
+            sys.exit()
+
+
     for i in range(0, len(platforms) - 2, 3):
         x, y, length = platforms[i], platforms[i + 1], platforms[i + 2]
         for j in range(x, x + length):
